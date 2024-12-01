9dc5d45f9b84f30ae3cd0d46e05333baf34ae357
shovon-gt <shovon.biswas@gigatechltd.com>
2024-12-01 23:44:51 +0600

    Use of Write, WriteLine and \n

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ class Test
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello C Sharp!");
+        Console.Write("My name is Md. Shovon Biswas!\n");
+        Console.WriteLine("I am 33 years old");
+        Console.WriteLine("I am a full stack developer");
     }
 }
