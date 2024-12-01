2f9237a31640fbca35da07c08a96974db4f35df6
shovon-gt <shovon.biswas@gigatechltd.com>
2024-12-01 23:55:33 +0600

    Used variable

--- Program.cs
+++ Program.cs
@@ -3,7 +3,11 @@ class Test
 {
     public static void Main(string[] args)
     {
-        // Print Hello world message.
-        Console.WriteLine("Hello C Sharp!");
+        // Use of variable.
+        string name = "Shovon Biswas";
+        Console.WriteLine(name);
+        Console.WriteLine(name);
+        Console.WriteLine(name);
+        Console.WriteLine(name);
     }
 }
--- obj/Debug/net8.0/C--Basic.AssemblyInfo.cs
+++ obj/Debug/net8.0/C--Basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C--Basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb551cf7420210933f672a28f01d35fae89ba56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634f4a7d91286d7b04696721b26e1712ee8e1361")]
 [assembly: System.Reflection.AssemblyProductAttribute("C--Basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C--Basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
