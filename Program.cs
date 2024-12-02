cfc1b403242e6ee0c9bdc123a9bbb4e50cbfee32
shovon-gt <shovon.biswas@gigatechltd.com>
2024-12-03 02:20:10 +0600

    Tyoe Casting

--- Program.cs
+++ Program.cs
@@ -3,17 +3,19 @@ class Test
 {
     public static void Main(string[] args)
     {
-        // Use of variable.
-        const string myName = "Shovon Biswas";
-        int age = 26;
-        double cgpa = 3.74;
-        char firstNameLetter = 'S';
-        bool goodPersonality = true;
+        //Type Casting
+        double salary = 2547.89;
+        Console.WriteLine(salary);
+        int salary2 = (int)salary;
+        Console.WriteLine(salary2);
 
-        Console.WriteLine(myName);
-        Console.WriteLine(age);
-        Console.WriteLine(cgpa);
-        Console.WriteLine(firstNameLetter);
-        Console.WriteLine(goodPersonality);
+        Console.WriteLine(Convert.ToString(salary));
+
+
+        //Implicit / Auto converion
+        //char -> int -> long -> float -> double
+
+        // Explicit / manual conversion (have to tell compiler to convert)
+        //double -> float -> long -> int -> char
     }
 }
--- obj/Debug/net8.0/C--Basic.AssemblyInfo.cs
+++ obj/Debug/net8.0/C--Basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C--Basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699d573f4334258843b109adeadf62f5b9b5ddb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72625c437375907afb6a96336ea922702a9900e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("C--Basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C--Basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
